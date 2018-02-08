674218e097cafb0957b7f1977dcd4269cc6bae5b
BeneLiza <35993953+BeneLiza@users.noreply.github.com>
2018-02-08 12:23:40 +0100

    Add files via upload

---
+++ tokens_for_Stanford_input.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def tokens_for_Stanford_input(tml_token_file):
+    """Function that takes a tml_token file (where sep=\|), extracts tokens and returns a conll file for Stanford
+    dependency parser """
+    df_tokens = pd.read_csv(tml_token_file, sep='\|', header=None).dropna(axis=1)
+    # Converts to dataframe
+    df_tokens.columns = ["article_id", "sentence_id", "token_id", "token", "na1", "na2", "na3"]
+    # Assigns headings
+    df_tokens["token"] = df_tokens["token"].str.replace("'", "")
+    # Removes apostrophes
+    token_string = df_tokens['token'].to_string(index=False)
+    # Converts token column into string
+    removed_enters = token_string.replace('\n', ' ')
+    final_tokens = ' '.join(removed_enters.split())
+    # Makes sure that there is only one white space between tokens
+    with open('stanford_input.txt', 'w', encoding='utf8') as outfile:
+        outfile.write(final_tokens)
+
+tokens_for_Stanford_input('tml_tokens.txt')
